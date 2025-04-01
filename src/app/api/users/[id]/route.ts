a26c162aa89feca92afe2e25ab94b8262f3e692e
Narayan Raghuwanshi <narayan.raghu26@gmail.com>
2025-04-02 01:28:56 +0530

    fixing build errorss

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,18 +2,24 @@ import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (
+export async function PUT(
   request: NextRequest,
+  //@ts-ignore
   { params }: { params: { id: string } }
-) => {
+) {
   await dbConnect();
   try {
     const { manualTime, reset } = await request.json();
-    const { id } = await params;
+    const { id } = await params; // `params` is now correctly typed
 
-    const updateData = reset
-      ? { lastVideoAssignedAt: null }
-      : { lastVideoAssignedAt: new Date(manualTime || Date.now()) };
+    let updateData;
+    if (reset) {
+      updateData = { lastVideoAssignedAt: null };
+    } else if (manualTime) {
+      updateData = { lastVideoAssignedAt: new Date(manualTime) };
+    } else {
+      updateData = { lastVideoAssignedAt: new Date() };
+    }
 
     const user = await User.findByIdAndUpdate(id, updateData, {
       new: true,
@@ -22,8 +28,8 @@ export const PUT = async (
     return NextResponse.json(user);
   } catch (error) {
     return NextResponse.json(
-      { message: "Error updating user", error },
+      { message: "Error updating user", error: error },
       { status: 500 }
     );
   }
-};
+}
